f9f0527ccd92a02f900d4e7cb93f29ee1ba93e7b
Thet Naing Tun <thetnaingtun@bindez.com>
2021-10-05 15:45:37 +0630

    implement countdown with observable

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { interval } from "rxjs";
+import { scan, share, startWith } from "rxjs/operators";
+
 import "./App.css";
 
+const TIME_LEFT = 25 * 60;
+
+const countdown$ = interval(1000).pipe(
+  startWith(TIME_LEFT),
+  scan((time) => {
+    if (time >= 1) return time - 1;
+    return 0;
+  }),
+  share()
+);
+
+function padTime(time: number) {
+  return time.toString().padStart(2, "0");
+}
+
 function App() {
+  const [timeLeft, setTimeLeft] = useState(TIME_LEFT);
+  useEffect(() => {
+    const subscription = countdown$.subscribe((val) => setTimeLeft(val));
+    return () => subscription.unsubscribe();
+  }, []);
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft - minutes * 60;
   return (
     <div className="app">
       <h2>Pomodoro!</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{padTime(minutes)}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{padTime(seconds)}</span>
       </div>
 
       <div className="buttons">
